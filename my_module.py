ba10f9ff526367577827e802ee4e938edc94aa5e
deelorr <deelorr@gmail.com>
2024-01-19 15:45:52 -0800

    first commit

---
+++ my_module.py
@@ -0,0 +1,9 @@
+from math import pi
+
+def find_sq_foot(length, width):
+    area = length * width
+    return area
+
+def find_circum(radius):
+    circumference = 2 * pi * radius
+    return circumference
